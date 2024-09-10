068e82fa12e2e26d2a11fe63bbd3f68de43e7973
양준호 <didwnsgugh@gmail.com>
2024-09-11 02:26:24 +0900

    학원에서 배운 내용 통합(이전에 빼먹었음)

--- src/main/java/com/test/lesson01/filter/CharacterEncodingFilter.java
+++ src/main/java/com/test/lesson01/filter/CharacterEncodingFilter.java
@@ -9,15 +9,18 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
-@WebFilter("/*") // 모든 주소에 대해 필터를 연결함.
+@WebFilter("/*") // => 모든 주소에 대해 필터링할 것임.
 public class CharacterEncodingFilter implements Filter { // import 시에 javax.servlet.Filter 선택.
-// extends가 아니라 implements임.
+	// extends가 아니라 implements임.
+	// 클래스명에 뜨는 에러 눌러서 Override 추가하기
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		
+		// 필터를 통과할 때 인코딩
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("utf-8");
-		chain.doFilter(request, response);
+		chain.doFilter(request, response); // 이거 빼먹으면 적용 안 됨.
 	}
 
 }
